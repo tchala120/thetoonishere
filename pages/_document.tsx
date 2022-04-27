383b81cc2b02134b6c5eb6ae24f12b38ea028415
Panupong Tipjoi <panupong.ti@hotmail.com>
2022-04-28 06:20:34 +0700

    feat: initiated project

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document: NextPage = () => {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
