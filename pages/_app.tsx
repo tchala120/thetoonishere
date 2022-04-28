7b00ec8b21caa61d9fe3148832ca1f9ebfd57df7
Panupong Tipjoi <panupong.ti@hotmail.com>
2022-04-28 07:05:28 +0700

    feat: Used config component large

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,14 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
+import { ConfigProvider } from 'antd'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider componentSize="large">
+      <Component {...pageProps} />
+    </ConfigProvider>
+  )
 }
 
 export default MyApp
