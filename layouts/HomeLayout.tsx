7ca4934e87a70b4dc5e1dcaa7ac7d3152f0cb439
Panupong Tipjoi <panupong.ti@hotmail.com>
2022-04-28 07:05:04 +0700

    feat: Add home layout

--- layouts/HomeLayout.tsx
+++ layouts/HomeLayout.tsx
@@ -1,7 +1,18 @@
-import { FC } from 'react'
+import styled from 'styled-components'
 
-const HomeLayout: FC = () => {
-  return <></>
+import type { FCWithChildren } from 'types'
+
+const HomeLayout: FCWithChildren = ({ children }) => {
+  return <HomeContainer>{children}</HomeContainer>
 }
 
 export default HomeLayout
+
+const HomeContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
