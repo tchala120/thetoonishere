45d2baa64c441cecfd628f4a0f30427f67f2fa44
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-04-30 18:29:30 +0700

    feat: Test image from external url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { GetStaticProps, NextPage } from 'next'
 
 import Image from 'next/image'
+import { Space } from 'antd'
 
 import HomeLayout from 'layouts/HomeLayout'
 
@@ -21,7 +22,16 @@ interface HomeProps {
 const Home: NextPage<HomeProps> = ({ listInstagramPosts }) => {
   return (
     <HomeLayout>
-      <Image src="/favicon.webp" alt="Panupong" width={64} height={64} />
+      <Space>
+        <Image src="/favicon.webp" alt="Panupong" width={64} height={64} />
+        <Image
+          src="https://scontent.cdninstagram.com/v/t51.29350-15/121709813_104503418057733_7809674863864946371_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=X93ZgSnPAlUAX_ffrYT&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT-1sBjzsVRMVTZMTIvpyETeInz-g50Jw9sYUPA_RVtxBg&oe=6272BA00"
+          alt="Democracy"
+          width={64}
+          height={64}
+          objectFit="cover"
+        />
+      </Space>
 
       <Hero />
 
