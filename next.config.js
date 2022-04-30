376a4e9c8a776413cc8c4521873613b9b9fd0a35
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-04-30 15:19:02 +0700

    feat: Accept domain for image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['scontent.cdninstagram.com'],
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
