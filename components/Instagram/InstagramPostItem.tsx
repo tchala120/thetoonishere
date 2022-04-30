3cb282f316923d01696843d198e13acb0f5776f6
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-04-30 18:09:50 +0700

    fix: Used html img instead

--- components/Instagram/InstagramPostItem.tsx
+++ components/Instagram/InstagramPostItem.tsx
@@ -1,6 +1,5 @@
 import type { FC } from 'react'
 
-import NextImage from 'next/image'
 import styled from 'styled-components'
 
 import type { InstagramAPIResponse } from 'pages/api/list-instagram-posts'
@@ -13,14 +12,7 @@ const InstagramPostItem: FC<InstagramPostItemProps> = ({ data }) => {
   return (
     <div style={{ overflow: 'hidden' }}>
       <a href={data.permalink} target="_blank" rel="noopener noreferrer">
-        <Image
-          src={data.media_url}
-          alt={data.timestamp}
-          width="100%"
-          height="100%"
-          layout="responsive"
-          objectFit="cover"
-        />
+        <Image src={data.media_url} alt={data.timestamp} />
       </a>
     </div>
   )
@@ -28,7 +20,9 @@ const InstagramPostItem: FC<InstagramPostItemProps> = ({ data }) => {
 
 export default InstagramPostItem
 
-const Image = styled(NextImage)`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
   aspect-ratio: 1/1;
   transition: all 0.25s ease-in-out;
   transform-origin: center;
