f1c8cf35c004a7465baac19b379d23fd6dd21626
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-04-30 15:19:27 +0700

    feat: Add loading icon component

---
+++ components/LoadingIcon.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { LoadingOutlined } from '@ant-design/icons'
+
+const LoadingIcon = styled(LoadingOutlined)`
+  display: block;
+  font-size: 54px;
+  margin: 36px auto;
+`
+
+export default LoadingIcon
