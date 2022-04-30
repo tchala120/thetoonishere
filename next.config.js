6d4fbc706318daeb46ca6f18a9c549335b390739
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-04-30 18:13:50 +0700

    chore: Remove bundle analyze

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer({
+const nextConfig = {
   images: {
     domains: ['scontent.cdninstagram.com'],
   },
@@ -12,4 +8,6 @@ module.exports = withBundleAnalyzer({
     styledComponents: true,
     removeConsole: process.env.NODE_ENV === 'production',
   },
-})
+}
+
+module.exports = nextConfig
