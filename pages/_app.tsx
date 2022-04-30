60c89a5030c523b7cd281e2afdca022891a31ff4
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-01 06:33:16 +0700

    feat: Add first load loading state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,22 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
+import { useEffect, useState } from 'react'
 import { ConfigProvider } from 'antd'
 
+import FullScreenLoading from 'components/FullScreenLoading'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(false)
+  }, [])
+
+  if (loading) {
+    return <FullScreenLoading>Loading...</FullScreenLoading>
+  }
+
   return (
     <ConfigProvider componentSize="large">
       <Component {...pageProps} />
