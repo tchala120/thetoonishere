a899ae4deca8fa2f9ca607ea28ea92de1cd35583
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-04-30 17:02:58 +0700

    feat: Used `next/image` instead

--- components/Instagram/InstagramPostItem.tsx
+++ components/Instagram/InstagramPostItem.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react'
 
+import NextImage from 'next/image'
 import styled from 'styled-components'
 
 import type { InstagramAPIResponse } from 'pages/api/list-instagram-posts'
@@ -12,7 +13,14 @@ const InstagramPostItem: FC<InstagramPostItemProps> = ({ data }) => {
   return (
     <div style={{ overflow: 'hidden' }}>
       <a href={data.permalink} target="_blank" rel="noopener noreferrer">
-        <Image src={data.media_url} alt={data.timestamp} />
+        <Image
+          src={data.media_url}
+          alt={data.timestamp}
+          width="100%"
+          height="100%"
+          layout="responsive"
+          objectFit="cover"
+        />
       </a>
     </div>
   )
@@ -20,10 +28,7 @@ const InstagramPostItem: FC<InstagramPostItemProps> = ({ data }) => {
 
 export default InstagramPostItem
 
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+const Image = styled(NextImage)`
   aspect-ratio: 1/1;
   transition: all 0.25s ease-in-out;
   transform-origin: center;
