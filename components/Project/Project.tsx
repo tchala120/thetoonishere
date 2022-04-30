30239ee259378074b0da87fb755290d1b948514f
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-04-30 15:43:28 +0700

    feat: Add project component

---
+++ components/Project/Project.tsx
@@ -0,0 +1,15 @@
+import type { FC } from 'react'
+
+import Section from 'components/Section'
+
+const Project: FC = () => {
+  return (
+    <Section>
+      <Section.Title>My previous works</Section.Title>
+
+      <h1>Coming Soon</h1>
+    </Section>
+  )
+}
+
+export default Project
