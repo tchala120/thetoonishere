d1fc400b8f62ab5cbec091114e6ee9b5f1382013
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-04-30 15:42:44 +0700

    feat: Add about component

---
+++ components/About.tsx
@@ -0,0 +1,15 @@
+import type { FC } from 'react'
+
+import Section from 'components/Section'
+
+const About: FC = () => {
+  return (
+    <Section>
+      My name is <strong>Toon</strong>, and here is my portfolio, which
+      showcases everything I&apos;ve done in my spare time. My ambition is to
+      work as a website developer since I enjoy coding.
+    </Section>
+  )
+}
+
+export default About
