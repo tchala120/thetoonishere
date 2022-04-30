5c6743d1e184734e7dca036a77a39a91f5b521ab
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-04-30 15:43:37 +0700

    feat: Add seo title and description

--- layouts/HomeLayout.tsx
+++ layouts/HomeLayout.tsx
@@ -4,10 +4,22 @@ import Content from 'components/Content'
 import Footer from 'components/Footer'
 
 import type { FCWithChildren } from 'types'
+import { NextSeo } from 'next-seo'
 
-const HomeLayout: FCWithChildren = ({ children }) => {
+interface HomeLayoutProps {
+  title?: string
+  description?: string
+}
+
+const HomeLayout: FCWithChildren<HomeLayoutProps> = ({
+  title = 'Hi! | thetoonishere',
+  description = "Toon's personal website",
+  children,
+}) => {
   return (
     <>
+      <NextSeo title={title} description={description} />
+
       <Head>
         <title>👋 Hi! | thetoonishere</title>
         <link rel="shortcut icon" href="/favicon.png" />
