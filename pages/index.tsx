9c05055aa17f74f4243194b81e391e5aa7c39b76
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-04-30 15:43:52 +0700

    fix: Moved to another component instead

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,24 @@
 import type { NextPage } from 'next'
 
-import { Space } from 'antd'
-import styled from 'styled-components'
-
 import HomeLayout from 'layouts/HomeLayout'
 
-import WaveAnimation from 'components/WaveAnimation'
+import Hero from 'components/Hero'
+import About from 'components/About'
 import Instagram from 'components/Instagram'
-import Section from 'components/Section'
+import Project from 'components/Project'
 
 const Home: NextPage = () => {
   return (
     <HomeLayout>
-      <Space size="large">
-        <WaveAnimation style={{ fontSize: 80 }}>👋</WaveAnimation>
-        <GreetingText>Hi!</GreetingText>
-      </Space>
+      <Hero />
 
-      <Section>
-        My name is <strong>Toon</strong>, and here is my portfolio, which
-        showcases everything I&apos;ve done in my spare time. My ambition is to
-        work as a website developer since I enjoy coding.
-      </Section>
+      <About />
 
       <Instagram />
+
+      <Project />
     </HomeLayout>
   )
 }
 
 export default Home
-
-const GreetingText = styled.span`
-  font-size: 80px;
-  font-weight: 900;
-`
