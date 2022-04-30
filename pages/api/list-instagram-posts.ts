a575a21f6d0268bbe928539abdcd976a041cf5f4
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-01 06:12:45 +0700

    feat: Handle cache

--- pages/api/list-instagram-posts.ts
+++ pages/api/list-instagram-posts.ts
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import fs from 'fs'
 import axios from 'axios'
+import path from 'path'
+import dayjs from 'dayjs'
 
 export interface InstagramAPIResponse {
   id: string
@@ -13,15 +16,49 @@ export interface ListInstagramPostsAPIPayload {
   listInstagramPosts: InstagramAPIResponse[]
 }
 
+export interface StaticInstagramPost {
+  data: InstagramAPIResponse[]
+  createdAt: number
+}
+
+const age = 600000 // 10 minutes
+const filePath = path.join(__dirname, '../../../..', '/posts.json')
+
 export const getListInstagramPosts = async (): Promise<
   InstagramAPIResponse[]
-> =>
-  axios
+> => {
+  const file = fs.readFileSync(filePath, 'utf-8')
+
+  const staticInstagramPost: StaticInstagramPost | null =
+    file == '' ? null : JSON.parse(file)
+
+  if (
+    staticInstagramPost != null &&
+    dayjs().isBefore(dayjs(staticInstagramPost.createdAt))
+  ) {
+    return new Promise((resolve) => resolve(staticInstagramPost.data))
+  }
+
+  return axios
     .get<{ data: InstagramAPIResponse[] }>(
       `https://graph.instagram.com/me/media?fields=id,media_url,permalink,timestamp&access_token=${process.env.INSTAGRAM_TOKEN}`
     )
-    .then((resp) => resp.data.data.slice(0, 9))
+    .then(async (resp) => {
+      fs.writeFileSync(
+        filePath,
+        JSON.stringify({
+          data: resp.data.data.slice(0, 9),
+          createdAt: Date.now() + age,
+        }),
+        {
+          encoding: 'utf-8',
+        }
+      )
+
+      return JSON.parse(fs.readFileSync(filePath, 'utf-8')).data
+    })
     .catch((error) => error)
+}
 
 const handler = async (_: NextApiRequest, res: NextApiResponse) => {
   try {
