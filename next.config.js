3aa97f4eae3ee14bdc22419af6fd7eeb9590db83
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-01 07:43:54 +0700

    chore: Remove unused packages and code

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'scontent.cdninstagram.com',
-      'scontent-iad3-1.cdninstagram.com',
-      'scontent-iad3-2.cdninstagram.com',
-      'scontent-bos3-1.cdninstagram.com',
-      'upload.wikimedia.org',
-    ],
+    domains: ['scontent.cdninstagram.com', 'upload.wikimedia.org'],
   },
   reactStrictMode: true,
   compiler: {
