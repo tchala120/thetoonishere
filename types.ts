34cb7210a655b87059600fe59b7a4c9b325b43e1
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-11 23:49:33 +0700

    feat: Add type for dynamic with layout

--- types.ts
+++ types.ts
@@ -1,3 +1,13 @@
-import { FC, PropsWithChildren } from 'react'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import type { FC, PropsWithChildren, ReactElement, ReactNode } from 'react'
+
+export type NextPageWithLayout<P = {}> = NextPage<P> & {
+  withLayout?: (page: ReactElement) => ReactNode
+}
+
+export type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
 
 export type FCWithChildren<P = {}> = FC<PropsWithChildren<P>>
