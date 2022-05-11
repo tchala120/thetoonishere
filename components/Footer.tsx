d2afa8b398997929d9545c308d00b994bac4d191
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-12 00:35:41 +0700

    feat: Add version

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,13 +3,23 @@ import type { FC } from 'react'
 import { CopyrightOutlined } from '@ant-design/icons'
 import { Space } from 'antd'
 import dayjs from 'dayjs'
+import styled from '@emotion/styled'
 
 const Footer: FC = () => {
   return (
-    <Space>
-      <CopyrightOutlined /> {dayjs().format('YYYY')} Panupong
-    </Space>
+    <>
+      <Space>
+        <CopyrightOutlined /> {dayjs().format('YYYY')} Panupong.
+      </Space>
+
+      <Version>Version {process.env.NEXT_PUBLIC_VERSION}</Version>
+    </>
   )
 }
 
 export default Footer
+
+const Version = styled.p`
+  margin-top: 8px;
+  color: #999;
+`
