5821af4691e3b09f4ca21da1e3d048c4060d22b5
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-12 00:08:28 +0700

    feat: Add error code description

---
+++ constants/error.ts
@@ -0,0 +1,8 @@
+const errorDescription: Record<number, string> = {
+  400: "You send the wrong data that we're don't understand.",
+  401: "You didn't have permission to access this site.",
+  404: 'The page you request not be found',
+  500: 'We are working and fixing this problem. Be back soon.',
+}
+
+export default errorDescription
