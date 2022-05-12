ec70ab955ebc18a4a2afeaaeef339791068629c9
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-13 02:36:37 +0700

    fix: Remove image domains

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['scontent.cdninstagram.com', 'upload.wikimedia.org'],
-  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
