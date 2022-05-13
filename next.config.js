60a86298a3fd0918eb6edbffed5cd368aefdc627
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-13 07:35:54 +0700

    feat: Adjust theming for antd

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
+const withPlugins = require('next-compose-plugins')
+const withAntdLess = require('next-plugin-antd-less')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
+  webpack(config) {
+    return config
+  },
 }
 
-module.exports = nextConfig
+const pluginAntdLess = withAntdLess({
+  modifyVars: {
+    '@primary-color': '#2F4858',
+    '@link-color': '#2F4858',
+  },
+})
+
+module.exports = withPlugins([[pluginAntdLess]], {
+  nextConfig,
+})
