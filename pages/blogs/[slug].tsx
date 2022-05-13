a5f57a4591fbed41fbb070e09a58cd5512d13163
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-13 07:28:45 +0700

    feat: Add blog detail page

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -1,9 +1,56 @@
-import type { NextPage } from 'next'
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+
+import { ParsedUrlQuery } from 'querystring'
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
 
 import PageLayout from 'layouts/PageLayout'
 
-const PostDetailPage: NextPage = () => {
-  return <PageLayout>Post Detail Page</PageLayout>
+import { Blog, getAllBlogs, getBlogContentBySlug } from 'helpers/blog'
+
+interface BlogDetailPageProps {
+  blog: Blog
+  source: MDXRemoteSerializeResult<Record<string, unknown>>
 }
 
-export default PostDetailPage
+const BlogDetailPage: NextPage<BlogDetailPageProps> = ({ blog, source }) => {
+  return (
+    <PageLayout title={blog.title} description={blog.excerpt}>
+      <MDXRemote {...source} />
+    </PageLayout>
+  )
+}
+
+export default BlogDetailPage
+
+interface BlogParams extends ParsedUrlQuery {
+  slug: string
+}
+
+export const getStaticProps: GetStaticProps<
+  BlogDetailPageProps,
+  BlogParams
+> = async ({ params }) => {
+  const blog = getBlogContentBySlug(params?.slug || '')
+  const mdxSource = await serialize(blog.content)
+
+  return {
+    props: {
+      blog,
+      source: mdxSource,
+    },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths<BlogParams> = () => {
+  const paths = getAllBlogs().map((blog) => ({
+    params: {
+      slug: blog.slug,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
