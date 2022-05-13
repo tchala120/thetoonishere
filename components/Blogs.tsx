ea27133b34ab12348909d2acd5c7b8f224dd279d
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-13 08:55:52 +0700

    feat: When blog not found should have message

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -7,6 +7,7 @@ import BlogCard from 'components/BlogCard'
 import PageLayout from 'layouts/PageLayout'
 
 import type { Blog } from 'helpers/blog'
+import NotFoundContent from './NotFoundContent'
 
 interface BlogsProps {
   blogs: Blog[]
@@ -18,15 +19,27 @@ const Blogs: FC<BlogsProps> = ({ blogs }) => {
       title="Blog | thetoonishere"
       description="Technical, lifestyle and more."
     >
-      {blogs.map((blog) => (
-        <Link key={blog.slug} href={`/blogs/${blog.slug}`}>
-          <a>
-            <BlogCard blog={blog} />
-          </a>
-        </Link>
-      ))}
+      {renderListBlogs()}
     </PageLayout>
   )
+
+  function renderListBlogs() {
+    if (blogs.length === 0) {
+      return (
+        <NotFoundContent>
+          <h1>Blog will be update soon.</h1>
+        </NotFoundContent>
+      )
+    }
+
+    return blogs.map((blog) => (
+      <Link key={blog.slug} href={`/blogs/${blog.slug}`}>
+        <a>
+          <BlogCard blog={blog} />
+        </a>
+      </Link>
+    ))
+  }
 }
 
 export default Blogs
