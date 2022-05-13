36bd3c10095cf4f13f649e5aded71e333f65a784
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-13 07:26:47 +0700

    feat: Add reading time

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -1,1 +1,10 @@
+import { convert } from 'html-to-text'
+import readingTime from 'reading-time'
+
 export const isDev = process.env.NODE_ENV === 'development'
+
+export const getBlogReadingTime = (content: string) => {
+  const text = convert(content)
+
+  return readingTime(text)
+}
