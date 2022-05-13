95cf52a3f8a41a0d2afa8218885f9511ba338e21
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-13 08:48:03 +0700

    feat: Add custom mdx components

---
+++ components/MDX.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const H1 = styled.h1`
+  font-weight: 900;
+  font-size: 32px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 24px;
+  }
+`
