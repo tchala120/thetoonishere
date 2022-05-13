565c1c9992086f46f07697852964eb2e74cd0e36
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-13 08:55:37 +0700

    feat: Add not found content component

---
+++ components/NotFoundContent.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+const NotFoundContent = styled.div`
+  width: 100%;
+  min-height: 60vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  > * {
+    font-weight: 900;
+  }
+`
+
+export default NotFoundContent
