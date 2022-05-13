e4349d1f1d0dee6dc76ab45eea2a8f54a8badad3
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-13 07:32:14 +0700

    chore: Remove `isDev` flag

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -1,8 +1,6 @@
 import { convert } from 'html-to-text'
 import readingTime from 'reading-time'
 
-export const isDev = process.env.NODE_ENV === 'development'
-
 export const getBlogReadingTime = (content: string) => {
   const text = convert(content)
 
