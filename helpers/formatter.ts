ef065a8126c08fd50230f9083fac6236b19dddb3
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-13 07:26:37 +0700

    feat: Added formatter about date

--- helpers/formatter.ts
+++ helpers/formatter.ts
@@ -1,3 +1,7 @@
+export const DATE_FORMAT = 'DD MMM, YYYY'
+export const TIME_FORMAT = 'HH:mm'
+export const DATE_TIME_FORMAT = `${DATE_FORMAT} - ${TIME_FORMAT} A`
+
 export function toEllipsisText(text = '', sliceLength = 20) {
   if (text.length < sliceLength) {
     return text
