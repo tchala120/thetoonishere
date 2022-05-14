f5274a73dcf9f33238ec665d82e2d28f20a8f313
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-15 06:29:18 +0700

    feat: Add medium blogs

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -3,25 +3,29 @@ import type { GetStaticProps, NextPage } from 'next'
 import dynamic from 'next/dynamic'
 
 import { getAllBlogs, Blog } from 'helpers/blog'
+import { getAllMediumBlogs, MediumBlog } from 'helpers/utils'
 
 const Blogs = dynamic(() => import('components/Blogs'))
 
 interface BlogPageProps {
-  blogs: Blog[]
+  listBlogs: Blog[]
+  listMediumBlogs: MediumBlog[]
 }
 
-const BlogPage: NextPage<BlogPageProps> = ({ blogs }) => {
-  return <Blogs blogs={blogs} />
+const BlogPage: NextPage<BlogPageProps> = ({ listBlogs, listMediumBlogs }) => {
+  return <Blogs listBlogs={listBlogs} listMediumBlogs={listMediumBlogs} />
 }
 
 export default BlogPage
 
-export const getStaticProps: GetStaticProps<BlogPageProps> = () => {
-  const blogs = getAllBlogs()
+export const getStaticProps: GetStaticProps<BlogPageProps> = async () => {
+  const listBlogs = getAllBlogs()
+  const listMediumBlogs = await getAllMediumBlogs()
 
   return {
     props: {
-      blogs,
+      listBlogs,
+      listMediumBlogs,
     },
   }
 }
