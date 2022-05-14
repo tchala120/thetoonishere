23e9808f1d955d0e1320ce9143698d139426739b
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-15 06:28:37 +0700

    feat: Add section for medium blog

--- components/Blogs/Blogs.tsx
+++ components/Blogs/Blogs.tsx
@@ -1,44 +1,66 @@
 import type { FC } from 'react'
 
 import Link from 'next/link'
+import { MediumSquareFilled } from '@ant-design/icons'
+import { Divider, Space } from 'antd'
 
+import Section from 'components/Section'
 import NotFoundContent from 'components/NotFoundContent'
 import BlogCard from './BlogCard'
+import MediumBlogCard from './MediumBlogCard'
 
 import PageLayout from 'layouts/PageLayout'
 
 import type { Blog } from 'helpers/blog'
+import type { MediumBlog } from 'helpers/utils'
 
 interface BlogsProps {
-  blogs: Blog[]
+  listBlogs: Blog[]
+  listMediumBlogs: MediumBlog[]
 }
 
-const Blogs: FC<BlogsProps> = ({ blogs }) => {
+const Blogs: FC<BlogsProps> = ({ listBlogs, listMediumBlogs }) => {
   return (
     <PageLayout
       title="Blog | thetoonishere"
       description="Technical, lifestyle and more."
     >
+      <Section>
+        <Section.SubTitle>
+          <Space size="large">
+            From Medium <MediumSquareFilled />
+          </Space>
+        </Section.SubTitle>
+
+        {listMediumBlogs.map((item) => (
+          <MediumBlogCard key={item.created} data={item} />
+        ))}
+      </Section>
+
       {renderListBlogs()}
     </PageLayout>
   )
 
   function renderListBlogs() {
-    if (blogs.length === 0) {
-      return (
-        <NotFoundContent>
-          <h1>Blog will be update soon.</h1>
-        </NotFoundContent>
-      )
+    if (listBlogs.length === 0) {
+      return <NotFoundContent />
     }
 
-    return blogs.map((blog) => (
-      <Link key={blog.slug} href={`/blogs/${blog.slug}`}>
-        <a>
-          <BlogCard blog={blog} />
-        </a>
-      </Link>
-    ))
+    return (
+      <>
+        <Divider />
+
+        <Section>
+          {listBlogs.map((blog) => (
+            <Link key={blog.slug} href={`/blogs/${blog.slug}`}>
+              <a>
+                <BlogCard blog={blog} />
+              </a>
+            </Link>
+          ))}
+        </Section>
+      </>
+    )
   }
 }
 
