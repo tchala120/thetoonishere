b636647ab542fce6c34ad19b1ac0fa098582230c
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-15 06:21:09 +0700

    refactor: Reduced duplicate codes

--- components/Blogs/BlogCard.tsx
+++ components/Blogs/BlogCard.tsx
@@ -1,13 +1,7 @@
 import type { FC } from 'react'
 
-import dayjs from 'dayjs'
-import styled from '@emotion/styled'
-
 import Section from 'components/Section'
-
-import { getBlogReadingTime } from 'helpers/utils'
-
-import { DATE_TIME_FORMAT } from 'helpers/formatter'
+import BlogHeader from 'components/BlogHeader'
 
 import type { Blog } from 'helpers/blog'
 
@@ -16,48 +10,14 @@ interface BlogCardProps {
 }
 
 const BlogCard: FC<BlogCardProps> = ({ blog }) => {
-  const readingTime = getBlogReadingTime(blog.content)
-
   return (
     <Section>
-      <BlogHeader>
-        <small className="date">
-          {dayjs(blog.date).format(DATE_TIME_FORMAT)}
-        </small>
-        <small className="reading-time">☕️ {readingTime.text}</small>
-      </BlogHeader>
+      <BlogHeader date={blog.date} content={blog.content} />
 
-      <BlogTitle>{blog.title}</BlogTitle>
-      <BlogExcerpt>{blog.excerpt}</BlogExcerpt>
+      <span className="blog-title">{blog.title}</span>
+      <p className="blog-description">{blog.excerpt}</p>
     </Section>
   )
 }
 
 export default BlogCard
-
-const BlogHeader = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  > .date {
-    font-weight: bold;
-  }
-`
-
-const BlogTitle = styled.span`
-  display: block;
-  font-size: 32px;
-  font-weight: 900;
-  margin-bottom: 16px;
-  color: #3c3c3c;
-
-  @media screen and (max-width: 768px) {
-    font-size: 28px;
-  }
-`
-
-const BlogExcerpt = styled.p`
-  font-size: 14px;
-`
