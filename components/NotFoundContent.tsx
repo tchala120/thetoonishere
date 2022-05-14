f9d2f2874eedcdf05823145fe550aaa5a8b9efc9
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-15 06:30:59 +0700

    fix: Changed min height of not found content

--- components/NotFoundContent.tsx
+++ components/NotFoundContent.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 
 const NotFoundContent = styled.div`
   width: 100%;
-  min-height: 60vh;
+  min-height: 40vh;
   display: flex;
   justify-content: center;
   align-items: center;
