8c56b33f48186fda32abaf7a70479f0c85130761
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-15 06:28:47 +0700

    feat: Add medium blog component

---
+++ components/Blogs/MediumBlogCard.tsx
@@ -0,0 +1,35 @@
+import type { FC } from 'react'
+
+import { Space, Tag } from 'antd'
+
+import Section from 'components/Section'
+import BlogHeader from 'components/BlogHeader'
+
+import { MediumBlog } from 'helpers/utils'
+
+interface MediumBlogCardProps {
+  data: MediumBlog
+}
+
+const MediumBlogCard: FC<MediumBlogCardProps> = ({ data }) => {
+  return (
+    <a href={data.link} target="_blank" rel="noopener noreferrer">
+      <Section>
+        <BlogHeader date={data.created} content={data.content} />
+
+        <span className="blog-title">{data.title}</span>
+        <p className="blog-description">{data.description}</p>
+
+        <Space>
+          {data.category.map((item) => (
+            <Tag color="purple" key={item}>
+              {item}
+            </Tag>
+          ))}
+        </Space>
+      </Section>
+    </a>
+  )
+}
+
+export default MediumBlogCard
