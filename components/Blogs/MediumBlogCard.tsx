cb9aa35b79a2dbea02cfdd821daeb448b2fef0a6
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-05-15 07:01:39 +0700

    feat: Link to backlink instead

--- components/Blogs/MediumBlogCard.tsx
+++ components/Blogs/MediumBlogCard.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react'
 
+import Link from 'next/link'
 import { Space, Tag } from 'antd'
 
 import Section from 'components/Section'
@@ -14,33 +15,32 @@ interface MediumBlogCardProps {
 
 const MediumBlogCard: FC<MediumBlogCardProps> = ({ data }) => {
   return (
-    <a
-      href={data.link}
-      target="_blank"
-      rel="noopener noreferrer"
-      onClick={() =>
-        event({
-          action: 'medium_blog',
-          category: 'medium',
-          label: data.title,
-        })
-      }
-    >
-      <Section>
-        <BlogHeader date={data.created} content={data.content} />
+    <Link href={`/medium-redirect/${data.slug}`}>
+      <a
+        onClick={() =>
+          event({
+            action: 'medium_blog',
+            category: 'medium',
+            label: data.title,
+          })
+        }
+      >
+        <Section>
+          <BlogHeader date={data.created} content={data.content} />
 
-        <span className="blog-title">{data.title}</span>
-        <p className="blog-description">{data.description}</p>
+          <span className="blog-title">{data.title}</span>
+          <p className="blog-description">{data.description}</p>
 
-        <Space>
-          {data.category.map((item) => (
-            <Tag color="purple" key={item}>
-              {item}
-            </Tag>
-          ))}
-        </Space>
-      </Section>
-    </a>
+          <Space>
+            {data.category.map((item) => (
+              <Tag color="purple" key={item}>
+                {item}
+              </Tag>
+            ))}
+          </Space>
+        </Section>
+      </a>
+    </Link>
   )
 }
 
